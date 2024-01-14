3e281d54a753902cf65e07121c6b8ccf5ed90ec4
GaleriaDelOlvido <156151357+GaleriaDelOlvido@users.noreply.github.com>
2024-01-14 11:00:44 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// Get the menu section
+const menuSection = document.getElementById("menu");
+
+// Add a click event listener to the menu section
+menuSection.addEventListener("click", function() {
+  // Scroll to the top of the page
+  window.scrollTo(0, 0);
+});
